b88a44bc265db3018fc5701296357b3b7a463cba
William del Rosario <williamdelrosario@yahoo.com>
2018-03-06 12:22:20 +0100

    Creating data fixtures

---
+++ src/AppBundle/DataFixtures/LoadMovieData.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: williamdelrosario
+ * Date: 3/6/18
+ * Time: 12:01 PM
+ */
+
+namespace AppBundle\DataFixtures;
+
+
+use AppBundle\Entity\Movie;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class LoadMovieData extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $movie1 = new Movie();
+        $movie1->setTitle('Green Mile');
+        $movie1->setYear(1999);
+        $movie1->setTime(   189);
+        $movie1->getDescription('Just a movie description');
+
+        $manager->persist($movie1);
+        $manager->flush();
+    }
+}
